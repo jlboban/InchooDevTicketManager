bc4f8a0d546aa575225a4a5c5ccf51272332c3ba
jlboban <jlboban17@gmail.com>
2021-01-15 11:31:37 +0100

    Migration update

--- src/Migration/Migration1610705287Ticket.php
+++ src/Migration/Migration1610705287Ticket.php
@@ -5,11 +5,11 @@ namespace InchooDev\TicketManager\Migration;
 use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Migration\MigrationStep;
 
-class Migration1610449243Ticket extends MigrationStep
+class Migration1610705287Ticket extends MigrationStep
 {
     public function getCreationTimestamp(): int
     {
-        return 1610449243;
+        return 1610705287;
     }
 
     public function update(Connection $connection): void
@@ -19,9 +19,13 @@ class Migration1610449243Ticket extends MigrationStep
               `id` BINARY(16) NOT NULL,
               `status` TINYINT(1) NOT NULL DEFAULT 1,
               `subject` VARCHAR(255) NOT NULL,
+              `content` VARCHAR(255) NOT NULL,
+              `customer_id` BINARY(16) NOT NULL,
               `created_at` DATETIME(3) NOT NULL,
               `updated_at` DATETIME(3) NULL,
-              PRIMARY KEY (`id`)
+              PRIMARY KEY (`id`),
+              CONSTRAINT `fk.inchoo_ticket.customer_id` FOREIGN KEY (`customer_id`)
+                REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
         ');
     }
--- src/Migration/Migration1610705295TicketReply.php
+++ src/Migration/Migration1610705295TicketReply.php
@@ -5,29 +5,26 @@ namespace InchooDev\TicketManager\Migration;
 use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Migration\MigrationStep;
 
-class Migration1610449248TicketMessage extends MigrationStep
+class Migration1610705295TicketReply extends MigrationStep
 {
     public function getCreationTimestamp(): int
     {
-        return 1610449248;
+        return 1610705295;
     }
 
     public function update(Connection $connection): void
     {
         $connection->executeUpdate('
-            CREATE TABLE IF NOT EXISTS `inchoo_ticket_message` (
+            CREATE TABLE IF NOT EXISTS `inchoo_ticket_reply` (
               `id` BINARY(16) NOT NULL,
               `content` VARCHAR(255) NOT NULL,
               `ticket_id` BINARY(16) NOT NULL,
-              `customer_id` BINARY(16) NOT NULL,
               `admin_id` BINARY(16),
               `created_at` DATETIME(3) NOT NULL,
               PRIMARY KEY (`id`),
-              CONSTRAINT `fk.ticket_message.ticket_id` FOREIGN KEY (`ticket_id`)
+              CONSTRAINT `fk.ticket_reply.ticket_id` FOREIGN KEY (`ticket_id`)
                 REFERENCES `inchoo_ticket` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-              CONSTRAINT `fk.ticket_message.customer_id` FOREIGN KEY (`customer_id`)
-                REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-              CONSTRAINT `fk.ticket_message.admin_id` FOREIGN KEY (`admin_id`)
+              CONSTRAINT `fk.ticket_reply.admin_id` FOREIGN KEY (`admin_id`)
                 REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
         ');
